94691fd930c7496a31583f4b6be59bbd2a36ecd4
Brian Frazho <befrazho@gmail.com>
2021-03-07 11:15:32 -0500

    handle file redirects for downloads

--- src/solipath_download/conditional_file_downloader.rs
+++ src/solipath_download/conditional_file_downloader.rs
@@ -41,21 +41,12 @@ impl ConditionalFileDownloaderTrait for ConditionalFileDownloader {
     }
     async fn download_and_decompress_file_if_directory_not_exists(&self, url: &str, directory_to_save_to: &Path) {
         if (!directory_to_save_to.exists()) {
-            let temp_dir = tempdir().unwrap();
-            let file_name = get_string_after_last_forward_slash(url);
-            let mut temp_file = temp_dir.into_path();
-            temp_file.push(file_name);
-            self.file_downloader.download_file(url, &temp_file).await;
+            let temp_dir = tempdir().unwrap().into_path();
+            let downloaded_file = self.file_downloader.download_file_to_directory(url, &temp_dir).await;
             self.file_decompressor
-                .decompress_file_to_directory(&temp_file, &directory_to_save_to);
+                .decompress_file_to_directory(&downloaded_file, &directory_to_save_to);
         }
     }
-}
-
-fn get_string_after_last_forward_slash(url: &str) -> String {
-    let index_of_forward_slash: usize = url.rfind('/').expect("could not find a forward slash in url");
-    let (_, string_after_last_slash) = url.split_at(index_of_forward_slash + 1);
-    string_after_last_slash.to_string()
 }
 
 #[cfg(test)]
@@ -65,12 +56,7 @@ mod tests {
     use super::*;
     use crate::solipath_download::file_decompressor::MockFileDecompressorTrait;
     use crate::solipath_download::file_downloader::MockFileDownloaderTrait;
-
-    #[test]
-    fn get_string_after_last_forward_slash_should_not_include_leading_slash() {
-        let url = "https://download.com/something.json";
-        assert_eq!(get_string_after_last_forward_slash(url), "something.json");
-    }
+    use std::path::PathBuf;
 
     #[tokio::test]
     async fn calls_file_downloader_when_file_does_not_exist() {
@@ -119,17 +105,18 @@ mod tests {
         let mut path_to_save_to = temp_dir.path().to_path_buf();
         path_to_save_to.push("directory_that_should_not_exist");
         let copy_path_to_save_to = path_to_save_to.clone();
+        
         let mut file_downloader = MockFileDownloaderTrait::new();
         file_downloader
-            .expect_download_file()
-            .withf(move |actual_url, actual_path| actual_url == url && actual_path.ends_with("download.zip"))
+            .expect_download_file_to_directory()
+            .withf(move |actual_url, _| actual_url == url)
             .times(1)
-            .return_const(());
+            .return_const(PathBuf::from("/output_path/download.zip"));
         let mut file_decompressor = MockFileDecompressorTrait::new();
         file_decompressor
             .expect_decompress_file_to_directory()
             .withf(move |source_file_path, target_directory| {
-                source_file_path.ends_with("download.zip") && target_directory == copy_path_to_save_to
+                source_file_path == PathBuf::from("/output_path/download.zip") && target_directory == copy_path_to_save_to
             })
             .times(1)
             .return_const(());
--- src/solipath_download/file_downloader.rs
+++ src/solipath_download/file_downloader.rs
@@ -1,6 +1,8 @@
 use async_trait::async_trait;
+use reqwest::Response;
 use reqwest::Client;
 use std::path::Path;
+use std::path::PathBuf;
 use tokio::{
     fs::{create_dir_all, File},
     io::AsyncWriteExt,
@@ -12,6 +14,7 @@ use mockall::{automock, predicate::*};
 #[cfg_attr(test, automock)]
 #[async_trait]
 pub trait FileDownloaderTrait {
+    async fn download_file_to_directory(&self, url: &str, directory_to_save_to: &Path)-> PathBuf;
     async fn download_file(&self, url: &str, path: &Path);
 }
 
@@ -25,22 +28,17 @@ impl FileDownloader {
             reqwest_client: Client::new(),
         }
     }
-}
 
-#[async_trait]
-impl FileDownloaderTrait for FileDownloader {
-    async fn download_file(&self, url: &str, path_to_save_to: &Path) {
-        let mut response = self
+    async fn make_request(&self, url: &str) -> Response {
+        self
             .reqwest_client
             .get(url)
             .send()
             .await
-            .expect("file download failed!");
-        let parent_directory = path_to_save_to.parent().unwrap();
-        create_dir_all(&parent_directory)
-            .await
-            .expect("failed to create parent directories");
-        let mut file = File::create(path_to_save_to).await.expect("could not create file");
+            .expect("file download failed!")
+    }
+    async fn stream_response_output_to_file(&self, response: &mut Response, file: &mut File) {
+        let url = response.url().as_str().to_string();
         println!("downloading {}...", url);
         while let Some(chunk) = response.chunk().await.expect("file download failed!") {
             file.write_all(&chunk)
@@ -51,30 +49,43 @@ impl FileDownloaderTrait for FileDownloader {
     }
 }
 
+#[async_trait]
+impl FileDownloaderTrait for FileDownloader {
+    async fn download_file_to_directory(&self, url: &str, directory_to_save_to: &Path)-> PathBuf {
+        let mut response = self.make_request(url).await;
+        create_dir_all(&directory_to_save_to).await.expect("failed to create directory");
+        let file_name = get_string_after_last_forward_slash(response.url().as_str());
+        let mut path_to_save_to = directory_to_save_to.to_path_buf();
+        path_to_save_to.push(file_name);
+        let mut file = File::create(path_to_save_to.clone()).await.expect("could not create file");
+        self.stream_response_output_to_file(&mut response, &mut file).await;
+        path_to_save_to
+    }
+
+    async fn download_file(&self, url: &str, path_to_save_to: &Path) {
+        let mut response = self.make_request(url).await;
+        let parent_directory = path_to_save_to.parent().unwrap();
+        create_dir_all(&parent_directory)
+            .await
+            .expect("failed to create parent directories");
+        let mut file = File::create(path_to_save_to).await.expect("could not create file");
+        self.stream_response_output_to_file(&mut response, &mut file).await;
+    }
+}
+
+fn get_string_after_last_forward_slash(url: &str) -> String {
+    let index_of_forward_slash: usize = url.rfind('/').expect("could not find a forward slash in url");
+    let (_, string_after_last_slash) = url.split_at(index_of_forward_slash + 1);
+    string_after_last_slash.to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use tempfile::tempdir;
     use tokio::fs::read_to_string;
 
-    #[tokio::test]
-    async fn can_download_a_file() {
-        let temp_dir = tempdir().unwrap().into_path();
-        let expected_file_path = temp_dir.join("LICENSE-MIT".to_string());
-        let file_downloader = FileDownloader::new();
-
-        file_downloader
-            .download_file(
-                "https://raw.githubusercontent.com/rust-lang/rust/master/LICENSE-MIT",
-                &expected_file_path,
-            )
-            .await;
-
-        let file_contents = read_to_string(expected_file_path.to_str().unwrap())
-            .await
-            .expect("something went wrong trying to read file");
-
-        let expected = r#"Permission is hereby granted, free of charge, to any
+    const DOWNLOAD_CONTENT: &str = r#"Permission is hereby granted, free of charge, to any
 person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the
 Software without restriction, including without
@@ -98,6 +109,51 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 "#;
-        assert_eq!(file_contents, expected);
+
+
+    #[test]
+    fn get_string_after_last_forward_slash_should_not_include_leading_slash() {
+        let url = "https://download.com/something.json";
+        assert_eq!(get_string_after_last_forward_slash(url), "something.json");
+    }
+
+    #[tokio::test]
+    async fn can_download_a_file() {
+        let temp_dir = tempdir().unwrap().into_path();
+        let expected_file_path = temp_dir.join("LICENSE-MIT".to_string());
+        let file_downloader = FileDownloader::new();
+
+        file_downloader
+            .download_file(
+                "https://raw.githubusercontent.com/rust-lang/rust/master/LICENSE-MIT",
+                &expected_file_path,
+            )
+            .await;
+
+        let file_contents = read_to_string(expected_file_path.to_str().unwrap())
+            .await
+            .expect("something went wrong trying to read file");
+
+        assert_eq!(file_contents, DOWNLOAD_CONTENT);
+    }
+
+
+    #[tokio::test]
+    async fn can_download_a_file_to_directory() {
+        let temp_dir = tempdir().unwrap().into_path();
+        let file_downloader = FileDownloader::new();
+
+        let actual_path = file_downloader
+            .download_file_to_directory(
+                "https://raw.githubusercontent.com/rust-lang/rust/master/LICENSE-MIT",
+                &temp_dir,
+            )
+            .await;
+
+        let file_contents = read_to_string(actual_path.to_str().unwrap())
+            .await
+            .expect("something went wrong trying to read file");
+
+        assert_eq!(file_contents, DOWNLOAD_CONTENT);
     }
 }
